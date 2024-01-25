a0371cb88e68f95c0f57e8e47ef38c731a7f7e12
Rupesh Saxena <>
2024-01-25 15:25:46 +0530

    Initial Commit

---
+++ SimpleCart/SimpleCart/SimpleCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCartApp.swift
+//  SimpleCart
+//
+//  Created by Rupesh Saxena on 25/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
